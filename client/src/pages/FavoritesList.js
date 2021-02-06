5336d468dfdd0f746e66fc3ad3cdd1bc13f5615f
Joshua Pribek <joshpribek@icloud.com>
2021-02-06 13:41:38 -0800

    favoritelist done

--- client/src/pages/FavoritesList.js
+++ client/src/pages/FavoritesList.js
@@ -2,23 +2,42 @@ import React, { useEffect } from "react";
 import { ListItem, List } from "../components/List";
 import DeleteBtn from "../components/DeleteBtn";
 import { Link } from "react-router-dom";
+import { useStoreContext } from "../utils/GlobalState";
+import { REMOVE_FAVORITE, LOADING, UPDATE_FAVORITES } from "../utils/actions";
 
-function FavoritesList() {
+const FavoritesList = () => {
+  const [state, dispatch] = useStoreContext();
+
+  const getFavorites = () => {
+    dispatch({ type: LOADING });
+    dispatch({ type: UPDATE_FAVORITES });
+  };
+
+  const removeFromFavorites = id => {
+    dispatch({
+      type: REMOVE_FAVORITE,
+      _id: id
+    });
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
   return (
     <div className="container mb-5 mt-5">
       <h1 className="text-center">Here's All of Your Favorite Posts</h1>
-      {/* Replace true with the condition that the posts array has a length > 0 */}
-      {true ? (
+      {state.favorites.length ? (
         <List>
           <h3 className="mb-5 mt-5">Click on a post to view in detail</h3>
-          {[].map(post => (
+          {state.favorites.map(post => (
             <ListItem key={post._id}>
               <Link to={"/posts/" + post._id}>
                 <strong>
                   {post.title} by {post.author}
                 </strong>
               </Link>
-              <DeleteBtn onClick={() => {}} />
+              <DeleteBtn onClick={() => removeFromFavorites(post._id)} />
             </ListItem>
           ))}
         </List>
@@ -30,6 +49,6 @@ function FavoritesList() {
       </div>
     </div>
   );
-}
+};
 
 export default FavoritesList;
